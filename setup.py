2782644541daab0f47ff4e2f023366be47f90798
elizabethriddle <32731629+elizabethriddle@users.noreply.github.com>
2022-03-01 10:56:38 +0000

    Update setup.py
    Added setup

--- setup.py
+++ setup.py
@@ -1,1 +1,9 @@
-
+from setuptools import find_packages, setup
+setup(
+    name='Simple-NMTF',
+    packages=find_packages(include=['Simple-NMTF']),
+    version='0.1.0',
+    description='',
+    author='Elizabeth Riddle-Workman',
+    license='MIT'
+)
