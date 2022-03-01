c8421fb13a3dc571810c7d08837a3b110362249c
elizabethriddle <32731629+elizabethriddle@users.noreply.github.com>
2022-03-01 09:53:01 +0000

    Update Multi_Graph_NMTF.py
    added Simple NMTF for newsgroup data

--- Simple-NMTF/Multi_Graph_NMTF.py
+++ Simple-NMTF/Multi_Graph_NMTF.py
@@ -220,3 +220,118 @@ def simple_multi_NMTF_NNDSVD_wo(A_CC,A_CU,A_CP,A_UU,cluster_sizes,sizes_of_attri
         return(G_C,G_U,G_P)
     else:
         return(G_C,G_U)
+    
+    
+def simple_multi_NMTF_NNDSVD_newsgroup(A_dw,A_dc,cluster_sizes,sizes_of_attributes,max_iter=100,verbose=False):
+    if not A_dw is None:
+        A_dw = csr_matrix(A_dw)
+    if not A_dc is None:
+        A_dc = csr_matrix(A_dc)
+    # Now set the initial clustering and values for G:
+    
+    # To make easier, will simply set as random assignment:
+    # Initialise using NNDSVD methodology:
+    stime = time.time()
+#     W_dw,H_dw = Multi_Graph_NMTF.NNDSVD_initialisation(A_dw.astype("float"),np.max(np.array([cluster_sizes[0],cluster_sizes[1]])))
+#     W_dc,H_dc = Multi_Graph_NMTF.NNDSVD_initialisation(A_dc.astype("float"),np.max(np.array([cluster_sizes[0],cluster_sizes[2]])))
+
+#     W_d = (W_dw[:,(np.max(np.array([cluster_sizes[0],cluster_sizes[1]]))-cluster_sizes[0]):np.max(np.array([cluster_sizes[0],cluster_sizes[1]]))]+W_dc[:,(np.max(np.array([cluster_sizes[0],cluster_sizes[2]]))-cluster_sizes[0]):np.max(np.array([cluster_sizes[0],cluster_sizes[2]]))])/2
+#     W_w = (H_dw.transpose())[:,(np.max(np.array([cluster_sizes[0],cluster_sizes[1]]))-cluster_sizes[1]):np.max(np.array([cluster_sizes[0],cluster_sizes[1]]))]
+#     W_c = H_dc.transpose()[:,(np.max(np.array([cluster_sizes[0],cluster_sizes[2]]))-cluster_sizes[2]):np.max(np.array([cluster_sizes[0],cluster_sizes[2]]))]
+
+#     doc_clusters = np.argmax(W_d,1)
+#     word_clusters = np.argmax(W_w,1)
+#     cat_clusters = np.argmax(W_c,1)
+
+    doc_clusters = np.random.randint(cluster_sizes[0],size=sizes_of_attributes[0])
+    word_clusters = np.random.randint(cluster_sizes[1],size=sizes_of_attributes[1])
+    cat_clusters = np.random.randint(cluster_sizes[2],size=sizes_of_attributes[2])
+
+    G_d = np.zeros((sizes_of_attributes[0],cluster_sizes[0]))
+    G_d[np.arange(G_d.shape[0]),doc_clusters] = 1.0
+    G_w = np.zeros((sizes_of_attributes[1],cluster_sizes[1]))
+    G_w[np.arange(G_w.shape[0]),word_clusters] = 1.0
+    G_c = np.zeros((sizes_of_attributes[2],cluster_sizes[2]))
+    G_c[np.arange(G_c.shape[0]),cat_clusters] = 1.0
+    # Now combine these along diagonal:
+    G_d = csr_matrix(G_d)
+    G_w = csr_matrix(G_w)
+    G_c = csr_matrix(G_c)
+    if verbose==True:
+        print("Initialise time,",time.time()-stime)
+
+    i=0
+    converged=False
+    number_repeats = 0
+    diff_G_d = 0
+    diff_G_w = 0
+    diff_G_c = 0
+    while (converged == False) & (i<max_iter):
+        G_d_old = G_d.copy()
+        G_w_old = G_w.copy()
+        G_c_old = G_c.copy()
+        diff_G_d_old = diff_G_d
+        diff_G_w_old = diff_G_w
+        diff_G_c_old = diff_G_c
+        # First find optimal S values:
+        # CC        
+        GTG_d = G_d.transpose().dot(G_d).toarray()+np.diag([1e-10]*np.sum(cluster_sizes[0]))
+        GTG_d_inv = inv(csr_matrix(GTG_d))
+        
+        # UU
+        GTG_w = G_w.transpose().dot(G_w).toarray()+np.diag([1e-10]*np.sum(cluster_sizes[1]))
+        GTG_w_inv = inv(csr_matrix(GTG_w))
+        # DW
+        S_dw = GTG_d_inv.dot(G_d.transpose().dot(A_dw.dot(G_w.dot(GTG_w_inv))))
+        # DC
+        GTG_c = G_c.transpose().dot(G_c).toarray()+np.diag([1e-10]*np.sum(cluster_sizes[2]))
+        GTG_c_inv = inv(csr_matrix(GTG_c))
+        S_dc = GTG_d_inv.dot(G_d.transpose().dot(A_dc.dot(G_c.dot(GTG_c_inv))))
+        
+        # Now need to find optimal G!
+        # Find pairwise distances:        
+        # Documents
+        G_dist_dw_d = pairwise_distances(A_dw,S_dw.dot(G_w.transpose()))
+        G_dist_dc_d = pairwise_distances(A_dc,S_dc.dot(G_c.transpose()))
+        G_d_choice = np.argmin(G_dist_dw_d+G_dist_dc_d,axis=1)
+        G_d = np.zeros((sizes_of_attributes[0],cluster_sizes[0]))
+        G_d[np.arange(G_d.shape[0]),G_d_choice] = 1.0
+        G_d = csr_matrix(G_d)
+        # Find for Words:
+        G_dist_dw_w = pairwise_distances(A_dw.transpose(),S_dw.transpose().dot(G_d.transpose()))
+        G_w_choice = np.argmin(G_dist_dw_w,axis=1)
+        G_w = np.zeros((sizes_of_attributes[1],cluster_sizes[1]))
+        G_w[np.arange(G_w.shape[0]),G_w_choice] = 1.0
+        G_w = csr_matrix(G_w)
+        # Find For cat
+        G_dist_dc_c = pairwise_distances(A_dc.transpose(),S_dc.transpose().dot(G_d.transpose()))
+        G_c_choice = np.argmin(G_dist_dc_c,axis=1)
+        G_c = np.zeros((sizes_of_attributes[2],cluster_sizes[2]))
+        G_c[np.arange(G_c.shape[0]),G_c_choice] = 1.0
+        G_c = csr_matrix(G_c)
+        i+=1
+        
+        diff_G_d = (G_d_old-G_d).power(2).sum()
+        diff_G_w = (G_w_old-G_w).power(2).sum()
+        diff_G_c = (G_c_old-G_c).power(2).sum()
+        
+        if (diff_G_d==diff_G_d_old) & (diff_G_w==diff_G_w_old):
+            number_repeats += 1
+            if number_repeats>4:
+                doc_stuck = np.where((G_d_old-G_d).power(2).sum(1)==2)[0]
+                word_stuck = np.where((G_w_old-G_w).power(2).sum(1)==2)[0]
+                if len(doc_stuck)>0:
+                    G_d[doc_stuck,:] = 0
+                    G_d[doc_stuck,[random.choice(range(0,cluster_sizes[0])) for i in range(len(doc_stuck))]] = 1
+                if len(word_stuck)>0:
+                    G_w[word_stuck,:] = 0
+                    G_w[word_stuck,[random.choice(range(0,cluster_sizes[1])) for i in range(len(word_stuck))]] = 1
+        else:
+            number_repeats = 0
+            
+        diff_G = (G_d_old-G_d).power(2).sum()+(G_w_old-G_w).power(2).sum()+(G_c_old-G_c).power(2).sum()
+        # Check if converged
+        if (diff_G<0.00000001):
+            converged = True
+    print("Number of iterations for convergence:",i)
+    return(G_d,G_w,G_c)
